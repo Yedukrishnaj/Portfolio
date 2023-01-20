c721b50d5b7c6ba912f3d337139456aa4658e61a
C22071833 <JayanY@cardiff.ac.uk>
2023-01-20 21:05:17 +0000

    final

--- website/__init__.py
+++ website/__init__.py
@@ -3,41 +3,40 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://c22071833:Yedujayan2000@csmysql.cs.cf.ac.uk:3306/c22071833_cmt120_cw2'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://c22071833:Yedujayan2000@csmysql.cs.cf.ac.uk:3306/c22071833_cmt120_cw2'
 
-    db.init_app(app)
+DB_NAME = "database.db"
+db = SQLAlchemy(app)
 
-    from .views import views
-    from .auth import auth
-
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+from .views import views
+from .auth import auth
 
 
-    from .models import User, Message
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
 
-    with app.app_context():
-        db.create_all()
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+from .models import User, Message
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+with app.app_context():
+    db.create_all()
 
-    return app
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 
 def create_database(app):
